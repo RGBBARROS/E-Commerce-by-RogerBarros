8aca6e2c7f9d2e66f9179fd882fb11e20778396e
RGBBARROS <107211438+RGBBARROS@users.noreply.github.com>
2024-01-10 16:10:40 -0300

    2 - Adaptação para polybalas

--- main.js
+++ main.js
@@ -2,65 +2,65 @@ const catalogo = [
     {
         id: 1,
         nome: "FINANCEIRO",
-        metragem: "DADOS DO SETOR FINANCEIRO",
-        preco: 100,
+        descricao: "DADOS DO SETOR FINANCEIRO",
+        responsavel: "VANESSA",
         nomeArquivoImagem: "dados financeiro.png",
     },
     
     {
-        id: 2,
-        nome: "COMERCIAL",
-        metragem: "DADOS DO SERT0R DO COMERCIAL",
-        preco: 200,
-        nomeArquivoImagem: "dados comercial.png",
+        id: 1,
+        nome: "FINANCEIRO",
+        descricao: "DADOS DO SETOR FINANCEIRO",
+        responsavel: "VANESSA",
+        nomeArquivoImagem: "dados financeiro.png",
     },
     
     {
-        id: 3,
-        nome: "LOGISTICA",
-        metragem: "DADOS DO SERT0R DE LOGISTICA",
-        preco: 300,
-        nomeArquivoImagem: "dados logistica.png",
+        id: 1,
+        nome: "FINANCEIRO",
+        descricao: "DADOS DO SETOR FINANCEIRO",
+        responsavel: "VANESSA",
+        nomeArquivoImagem: "dados financeiro.png",
     },
     
     {
-        id: 4,
-        nome: "APARTAMENTO",
-        metragem: "de 60m² até 100m²",
-        preco: 100,
-        nomeArquivoImagem: "image-4.jpg",
+        id: 1,
+        nome: "FINANCEIRO",
+        descricao: "DADOS DO SETOR FINANCEIRO",
+        responsavel: "VANESSA",
+        nomeArquivoImagem: "dados financeiro.png",
     },
     
     {
-        id: 5,
-        nome: "APARTAMENTO",
-        metragem: "de 200m² até 300m²",
-        preco: 300,
-        nomeArquivoImagem: "image-5.jpg",
+        id: 1,
+        nome: "FINANCEIRO",
+        descricao: "DADOS DO SETOR FINANCEIRO",
+        responsavel: "VANESSA",
+        nomeArquivoImagem: "dados financeiro.png",
     },
     
     {
-        id: 6,
-        nome: "APARTAMENTO",
-        metragem: "até 45m²",
-        preco: 50,
-        nomeArquivoImagem: "image-6.jpg",
+        id: 1,
+        nome: "FINANCEIRO",
+        descricao: "DADOS DO SETOR FINANCEIRO",
+        responsavel: "VANESSA",
+        nomeArquivoImagem: "dados financeiro.png",
     },
     
     {
-        id: 7,
-        nome: "APARTAMENTO",
-        metragem: "de 301m² até 400m²",
-        preco: 400,
-        nomeArquivoImagem: "image-7.jpg",
+        id: 1,
+        nome: "FINANCEIRO",
+        descricao: "DADOS DO SETOR FINANCEIRO",
+        responsavel: "VANESSA",
+        nomeArquivoImagem: "dados financeiro.png",
     },
     
     {
-        id: 8,
-        nome: "APARTAMENTO",
-        metragem: "de 101m² até 200m²",
-        preco: 200,
-        nomeArquivoImagem: "image-8.jpg",
+        id: 1,
+        nome: "FINANCEIRO",
+        descricao: "DADOS DO SETOR FINANCEIRO",
+        responsavel: "VANESSA",
+        nomeArquivoImagem: "dados financeiro.png",
     },
 ];
 
@@ -72,8 +72,8 @@ alt="Casa na Praia Laranja."
 style="height: 200px;"
 />
 <p>${produtoCatalogo.nome}</p>
-<p>${produtoCatalogo.metragem}</p>
-<p>R$${produtoCatalogo.preco}</p>
+<p>${produtoCatalogo.descricao}</p>
+<p>${produtoCatalogo.responsavel}</p>
 <button>Adicionar</button>
 </div>`;
 
